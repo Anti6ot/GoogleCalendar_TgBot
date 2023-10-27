00fbc558c99b58b02ba654131761fdcfcae9330c
Nikolai Antonov <94235876+Anti6ot@users.noreply.github.com>
2023-10-27 11:57:04 +0500

    Update calendar.js

--- src/common/calendar.js
+++ src/common/calendar.js
@@ -5,7 +5,7 @@ export default function calendar (bot,dataGcalendar, gglClndr, msg, chatId){
     gglClndr.startNavCalendar(msg) //Инлайн клавиатура календаря в чате 
     gglClndr.startTimeSelector(msg) //инлайн клавиатура установки времени
 
-    function proverka(msg) { //метод проверки литенера и отключения прослушки
+    function proverka(msg) { //метод проверки листенера и отключения прослушки
               dataGcalendar.text = msg.text
               run(dataGcalendar)
               bot.sendMessage(chatId, 'Событие было добавленно');
